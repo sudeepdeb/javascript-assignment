663d9dc63db1c24c8e250bed1f027ca93871e109
Sudeep Deb <sudeep@mastiska.com>
2016-12-30 18:31:20 +0530

    Count character

---
+++ count-character/count-char.js
@@ -0,0 +1,10 @@
+"Use strict"
+
+function countCharacter(s) {
+    var userString = new String(s);
+    console.log(userString);
+    var userStringLength = userString.length;
+    console.log(userStringLength);
+}
+
+countCharacter("This is Javascript");
